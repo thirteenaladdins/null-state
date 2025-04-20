0b671f7d66d6d31cf99bf7d1f8dfd22f848bf28a
Mo Amer <moamrr@gmail.com>
2025-04-20 20:45:44 +0100

    fix: remove duplicate router configuration

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import router from "./router";
 import "./style.css";
 import App from "./App.vue";
 
 import Landing from "./views/Landing.vue";
 import Profiles from "./views/Profiles.vue";
 import Logs from "./views/Logs.vue";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", component: Landing },
-    { path: "/profiles", component: Profiles },
-    { path: "/logs", component: Logs },
-  ],
-});
 
 const app = createApp(App);
 app.use(router);
